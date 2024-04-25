a23777712d773fc87ce9cd47658c828a0650fcae
samzhangw <87743920+samzhangw@users.noreply.github.com>
2024-04-26 06:31:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -94,4 +94,5 @@ def success(username):
     return render_template('welcome.html', username=username)
 
 if __name__ == "__main__":
-    app.run(debug=True, , port=4000)
+    app.run(debug=True, host='0.0.0.0', port=4000)
+
